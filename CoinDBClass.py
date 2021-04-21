2993643b77af789d60b3b62e84aa0779830de858
Snir Sharristh <s.sharristh@gmail.com>
2021-04-21 17:50:26 +0300

    first commit

---
+++ CoinDBClass.py
@@ -0,0 +1,43 @@
+import requests
+
+
+class CoinDB:
+    def __init__(self):
+        self._json = requests.get('http://api.coincap.io/v2/assets').json().get('data')
+        self.coins = {coin_dict.get('symbol'): coin_dict for coin_dict in self._json}
+
+    def refresh(self):
+        self._json = requests.get('http://api.coincap.io/v2/assets').json().get('data')
+        self.coins = {coin_dict.get('symbol'): coin_dict for coin_dict in self._json}
+
+    def get_coin_data(self, symbol):
+        # TODO: Pretty print the data:
+        return self.coins[symbol]
+
+    def get_price_usd(self, symbol):
+        return round(float(self.get_coin_data(symbol).get('priceUsd')), 2)
+
+    def get_change_24hr(self, symbol):
+        return round(float(self.get_coin_data(symbol).get('changePercent24Hr')), 2)
+
+    @staticmethod
+    def get_10m_notification_message() -> str:
+        # TODO: CREATE MESSAGE EVERY 10M
+        db = CoinDB()
+        btc_price = db.get_price_usd('BTC')
+        return f"{btc_price}"
+
+
+
+######
+# assets_request_response = requests.get('http://api.coincap.io/v2/assets')
+#
+# for coin_dict in assets_request_response.json().get('data'):
+#     coin_name = coin_dict.get('name')
+#     coin_symbol = coin_dict.get('symbol')
+#     coin_price_usd = coin_dict.get('priceUsd')
+#     coin_change_pct_24hr = coin_dict.get('changePercent24Hr')
+#     print("#########################################################")
+#     print(f"{coin_symbol} - {coin_name}")
+#     print(f"Current price ${round(float(coin_price_usd), 2)}")
+#     print(f"Coin change (Past 24Hrs) {round(float(coin_change_pct_24hr), 2)}%")
---
+++ main.py
@@ -0,0 +1,130 @@
+from CoinDBClass import CoinDB
+import os
+import logging
+import time
+
+from telegram import Update, ParseMode
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+API_TOKEN_TELEGRAM = os.environ['API_TOKEN_TELEGRAM']
+CRYPTO_GROUP_ID = int(os.environ['CRYTO_GROUP_CHAT_ID'])
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update: Update, _: CallbackContext) -> None:
+    update.message.reply_text('Hi! Use /set <seconds> to set a timer')
+
+
+def alarm(context: CallbackContext) -> None:
+    """Send the alarm message."""
+    job = context.job
+    context.bot.send_message(job.context, text='Beep!')
+
+
+def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
+def set_timer(update: Update, context: CallbackContext) -> None:
+    """Add a job to the queue."""
+    chat_id = update.message.chat_id
+    if chat_id != CRYPTO_GROUP_ID:
+        update.message.reply_text("WRONG GROUP BRUH")
+        return
+    try:
+        # args[0] should contain the time for the timer in seconds
+        due = int(context.args[0])
+        if due < 0:
+            update.message.reply_text('Sorry we can not go back to future!')
+            return
+
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_once(alarm, due, context=chat_id, name=str(chat_id))
+
+        text = 'Timer successfully set!'
+        if job_removed:
+            text += ' Old one was removed.'
+        update.message.reply_text(text)
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /set <seconds>')
+
+
+def unset(update: Update, context: CallbackContext) -> None:
+    """Remove the job if the user changed their mind."""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = 'Timer successfully cancelled!' if job_removed else 'You have no active timer.'
+    update.message.reply_text(text)
+
+
+def show_market(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+    if chat_id != CRYPTO_GROUP_ID:
+        update.message.reply_text("WRONG GROUP BRUH")
+        return
+    else:
+        cdb = CoinDB()
+        try:
+            coin_symbol = context.args[0]
+            update.message.reply_text(cdb.get_coin_data(coin_symbol.upper()))
+        except (IndexError, ValueError):
+            update.message.reply_text('Fuck you doing? just tell me the symbol')
+        except (KeyError):
+            update.message.reply_text('I dont know this coin man...')
+
+
+def market_notification_10m(bot):
+    while True:
+        bot.send_message(
+            CRYPTO_GROUP_ID,
+            f"{CoinDB.get_10m_notification_message()}",
+            parse_mode=ParseMode.HTML,
+
+        )
+        time.sleep(600)
+
+
+def main() -> None:
+    """Run bot."""
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(API_TOKEN_TELEGRAM)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("supwith", show_market))
+    market_notification_10m(dispatcher.bot)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Block until you press Ctrl-C or the process receives SIGINT, SIGTERM or
+    # SIGABRT. This should be used most of the time, since start_polling() is
+    # non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+    ###### EXAMPLES ######
+    # dispatcher.add_handler(CommandHandler("start", start))
+    # dispatcher.add_handler(CommandHandler("help", start))
+    # dispatcher.add_handler(CommandHandler("set", set_timer))
+    # dispatcher.add_handler(CommandHandler("unset", unset))
+
+
+if __name__ == '__main__':
+    main()
